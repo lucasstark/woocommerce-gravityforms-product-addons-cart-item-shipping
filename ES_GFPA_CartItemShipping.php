eb772d22a2d62007c123f88d8ae24c32c790eb3a
Lucas Stark <lucas.stark@live.com>
2022-07-18 22:28:17 -0400

    Process rules and apply mapped shipping class.

--- ES_GFPA_CartItemShipping.php
+++ ES_GFPA_CartItemShipping.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class ES_GFPA_CartItemShipping
  *
@@ -85,7 +86,7 @@ class ES_GFPA_CartItemShipping {
 			$new_shipping_class_id = $this->calculate_shipping_class( $gravity_form_data, $gravity_form_lead );
 
 			// Set the new calculated shipping class id
-			if ( $new_shipping_class_id ) {
+			if ( $new_shipping_class_id !== false ) {
 				$cart_item['shipping_class']['new'] = $new_shipping_class_id;
 				$cart_item['data']->set_shipping_class_id( $new_shipping_class_id );
 			}
@@ -121,9 +122,9 @@ class ES_GFPA_CartItemShipping {
 		$show              = $gravity_form_data['enable_cart_shipping_class_display'] ?? 'no';
 		if ( $show == 'yes' && isset( $cart_item['shipping_class'] ) ) {
 			// Display original shipping class name
-			if ( isset( $cart_item['shipping_class']['default'] ) ) {
+			if ( isset( $cart_item['shipping_class']['default'] ) &&  $cart_item['shipping_class']['default'] != 0 ) {
 				$default_shipping_class = ES_GFPA_CartItemShipping_Main::get_shipping_class_by_id( $cart_item['shipping_class']['default'] ?? false );
-				$item_data[] = array(
+				$item_data[]            = array(
 					'key'   => __( 'Shipping Class (default)', 'woocommerce' ),
 					'value' => $default_shipping_class ? $default_shipping_class->name : $cart_item['shipping_class']['default']
 				);
@@ -132,7 +133,7 @@ class ES_GFPA_CartItemShipping {
 			// Display new shipping class name
 			if ( isset( $cart_item['shipping_class']['new'] ) ) {
 				$new_shipping_class = ES_GFPA_CartItemShipping_Main::get_shipping_class_by_id( $cart_item['shipping_class']['new'] ?? false );
-				$item_data[] = array(
+				$item_data[]        = array(
 					'key'   => __( 'Shipping Class (new)', 'woocommerce' ),
 					'value' => $new_shipping_class ? $new_shipping_class->name : $cart_item['shipping_class']['new']
 				);
@@ -173,6 +174,7 @@ class ES_GFPA_CartItemShipping {
 	}
 
 	private function calculate_shipping_class( $gravity_form_data, $gravity_form_lead ) {
+
 		$form_meta = RGFormsModel::get_form_meta( $gravity_form_data['id'] );
 
 		//Something wrong with the form, just return the cart item.
@@ -182,24 +184,89 @@ class ES_GFPA_CartItemShipping {
 
 		$new_shipping_class_id   = false;
 		$shipping_class_field_id = $gravity_form_data['cart_shipping_class_field'] ?? false;
-		if ( $shipping_class_field_id !== false ) {
 
-			if ( isset( $gravity_form_lead[ $shipping_class_field_id ] ) ) {
-				$field = GFAPI::get_field( $gravity_form_data['id'], $shipping_class_field_id );
-				$logic = $field->conditionalLogic;
-				$value = RGFormsModel::get_lead_field_value( $gravity_form_lead, $field );
-
-				// use lead field display so that values are properly gathered from product and product option fields.
-				$display_value         = GFCommon::get_lead_field_display( $field, $value, $gravity_form_lead["currency"] ?? false );
-				$new_shipping_class_id = $this->map_value_to_shipping_class( $display_value );
+		if ( isset( $gravity_form_data['cart_shipping_mappings'] ) ) {
+			$shipping_classes = ES_GFPA_CartItemShipping_Main::get_shipping_classes();
+			$shipping_classes = wp_list_pluck($shipping_classes, 'term_id', 'slug');
+			foreach ( $gravity_form_data['cart_shipping_mappings'] as $key => $mapping ) {
+				if ( $mapping['conditionalLogic']['enabled'] !== 'yes' ) {
+					continue;
+				}
+				if ( $this->evaluate_conditional_logic( $form_meta, $mapping['conditionalLogic'], [], $gravity_form_lead ) ) {
+					$new_shipping_class_id = $shipping_classes[$key] ?? false;
+				}
 			}
+		} else if ( $shipping_class_field_id && isset( $gravity_form_lead[ $shipping_class_field_id ] ) ) {
+			$field = GFAPI::get_field( $gravity_form_data['id'], $shipping_class_field_id );
+			$value = RGFormsModel::get_lead_field_value( $gravity_form_lead, $field );
+
+			// use lead field display so that values are properly gathered from product and product option fields.
+			$display_value         = GFCommon::get_lead_field_display( $field, $value, $gravity_form_lead["currency"] ?? false );
+			$new_shipping_class_id = $this->map_value_to_shipping_class( $display_value );
 		}
+
 
 		return $new_shipping_class_id;
 	}
 
 	private function map_value_to_shipping_class( $value ) {
 		return apply_filters( 'wc_gfpa_map_value_to_shipping_class', $value );
+	}
+
+
+	private function evaluate_conditional_logic( $form, $logic, $field_values, $entry = null ) {
+
+		if ( empty( $logic ) ) {
+			return true;
+		}
+
+		$match_count = 0;
+		foreach ( $logic['rules'] as $rule ) {
+			try {
+				/**
+				 * Filter the conditional logic rule before it is evaluated.
+				 *
+				 * @param array $rule The conditional logic rule about to be evaluated.
+				 * @param array $form The current form meta.
+				 * @param array $logic All details required to evaluate an objects conditional logic.
+				 * @param array $field_values The default field values for this form.
+				 * @param array $entry The current entry object (if available).
+				 *
+				 * @since 2.4.22
+				 *
+				 */
+				$rule = apply_filters( 'gform_rule_pre_evaluation', $rule, $form, $logic, $field_values, $entry );
+			} catch ( Error $e ) {
+				GFCommon::log_error( __METHOD__ . '(): Error from function hooked to gform_rule_pre_evaluation. ' . $e->getMessage() );
+			}
+			$source_field = RGFormsModel::get_field( $form, $rule['fieldId'] );
+			$source_value = empty( $entry ) ? RGFormsModel::get_field_value( $source_field, $field_values ) : RGFormsModel::get_lead_field_value( $entry, $source_field );
+
+			/**
+			 * Filter the source value of a conditional logic rule before it is compared with the target value.
+			 *
+			 * @param int|string $source_value The value of the rule's configured field ID, entry meta, or custom property.
+			 * @param array $rule The conditional logic rule that is being evaluated.
+			 * @param array $form The current form meta.
+			 * @param array $logic All details required to evaluate an objects conditional logic.
+			 * @param array $entry The current entry object (if available).
+			 *
+			 * @since 2.6.2
+			 *
+			 */
+			$source_value = apply_filters( 'gform_rule_source_value', $source_value, $rule, $form, $logic, $entry );
+
+			$is_value_match = RGFormsModel::is_value_match( $source_value, $rule['value'], $rule['operator'], $source_field, $rule, $form );
+
+			if ( $is_value_match ) {
+				$match_count ++;
+			}
+		}
+
+		$do_action = ( $logic['logicType'] == 'all' && $match_count == sizeof( $logic['rules'] ) ) || ( $logic['logicType'] == 'any' && $match_count > 0 );
+		$is_hidden = ( $do_action && $logic['actionType'] == 'hide' ) || ( ! $do_action && $logic['actionType'] == 'show' );
+
+		return ! $is_hidden;
 	}
 
 }
